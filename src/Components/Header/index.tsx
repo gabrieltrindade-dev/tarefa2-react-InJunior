3cdb7999d5944e5a7b742ca7e8653ff6c23bb73d
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-12 02:04:08 -0300

    feat: Header

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import styles from "./styles.module.css"
+import logo from "../../assets/Logo.png"
+import pessoa from "../../assets/Icon.png"
+import carrinho from "../../assets/carrinho.png"
+
+export default function Header() {
+    return (
+        <header>
+            <nav className={styles.links}>
+                <Link to="/home"> <img src={logo} alt="Logo" /> </Link>
+                <div className={styles.icones}>
+                    <Link to="/" className={styles.fundoImg}> <img src={pessoa} alt="Icone pessoa" /> </Link>
+                    <Link to="/cart" className={styles.fundoImg}> <img src={carrinho} alt="Icone carrinho" /> </Link>
+                </div>
+            </nav>
+        </header>
+    )
+}
