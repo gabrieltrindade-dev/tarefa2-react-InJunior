c4ac2ddda20a97cc009f4d82e989471359716dda
Gabriel Trindade <gabrielt20514@gmail.com>
2025-08-14 03:02:32 -0300

    fix: ajuste de rota para pagina de cada livro

--- src/router.tsx
+++ src/router.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Themes />
       },
       {
-        path: "products",
+        path: "livro/:id",
         element: <Products />
       },
       {
